a7a48fa433fd5bc92d2f51dcf14e4e8735a0ecae
Matthew Baxter <matthewbaxter0412@hotmail.com>
2020-05-07 19:22:33 -0400

    added buttons that allow user to switch to photo tab and home tab. Created those html tabs as well

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask , render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
+
+@app.route('/to_photos/', methods=['POST'])
+def to_photos():
+    return render_template('photos.html')
+
+@app.route('/to_home/', methods=['POST'])
+def to_home():
+    return render_template('index.html')
+
+@app.route('/photos')
+def photos():
+    return render_template('photos.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
