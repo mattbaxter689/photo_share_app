4a888065717fe266a5689b103f45768f07115207
Matthew Baxter <matthewbaxter0412@hotmail.com>
2020-05-10 16:25:20 -0400

    fixed bug where on page refresh would ask for form resubmission. Now it automatically refreshes the page when prompted

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ def photos():
 #the 2 following routes are for buttons that allow users to move to the "Photos" tab, and back to the home page
 @app.route('/to_photos/', methods=['POST'])
 def to_photos():
-    return render_template('photos.html')
+    return redirect(url_for('photos'))
 
 @app.route('/to_home/', methods=['POST'])
 def to_home():
-    return render_template('index.html')
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
